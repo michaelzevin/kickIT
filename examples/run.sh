169d1adf5b2baca9339386b8eac481a672531de8
michaelzevin <mzevin1@gmail.com>
2019-03-30 12:01:59 -0500

    updated example executable script

--- examples/run.sh
+++ examples/run.sh
@@ -4,20 +4,19 @@ source activate sgrb-py36
 
 python /Users/michaelzevin/research/github/sgrb/run.py \
 --grb 070809 \
---t0 20 \
---Nsys 1 \
---Tsteps 100 \
---Rgrid 500 \
---Zgrid 300 \
---Rgrid-max 1000 \
---Zgrid-max 100 \
---Tint-max 180 \
---interp-dirpath /Users/michaelzevin/research/sgrb/interp_potentials \
+--Nsys 10 \
+--smhm-relation Moster \
+--smhm-sigma 0 \
 --sgrb-path /Users/michaelzevin/research/sgrb/data/sgrb_hostprops_offsets.txt \
 --samples-path /Users/michaelzevin/research/sgrb/data/example_bns.dat \
 --output-dirpath /Users/michaelzevin/research/sgrb/output_files/test \
 --verbose \
---multiproc max \
 --save-traj \
 --downsample 100 \
-#--sample-progenitor-props \
+--sample-progenitor-props \
+--interp-path /Users/michaelzevin/research/sgrb/interp_tests/interp.pkl \
+--Tint-max 120
+--multiproc max \
+#--fixed-birth 50 \
+#--fixed-potential 90 \
+#--differential-prof \
